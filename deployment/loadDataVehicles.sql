0950aad45a0e1c567bfe7e57f111b1afa9686eb4
FernandoSolísPaz <nanosolis@live.cl>
2024-06-06 19:32:09 -0400

    vehicle deployments added

---
+++ deployment/loadDataVehicles.sql
@@ -0,0 +1,6 @@
+INSERT INTO Car (plate, car_brand_id, model, type, year_of_fabrication, motor, number_of_seats, kilometers)
+VALUES 
+('CGZA96', 1, '4x4', 0, 2004, 0, 4, 40000),
+('PGKL64', 1, 'Corolla', 1, 2008, 1, 4, 30000),
+('HOLA11', 2, 'Wrangler', 2, 2006, 2, 2, 32000),
+('BYES43', 3, 'Frontier', 3, 2010, 3, 4, 17500);
